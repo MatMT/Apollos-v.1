91ac00575f5029d8df0bcdf207d7940b08eb413b
MatMT <oscarmateoelias@gmail.com>
2022-06-17 09:13:39 -0600

    1° Vista y Ruta + Info
    Se ha agregado una primera vista sin estilo css, y se ha especificado en gran parte información de la estructuración de las carpetas y acerca de las url

--- Apollos -  code/routes/web.php
+++ Apollos -  code/routes/web.php
@@ -5,14 +5,30 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
+
+    Route::get();
+    Route::post();
+    Route::patch();
+    Route::put();
+    Route::delete();
+    Route::options();
+
+    ---
+
+    // Responder a varios metodos en una misma ruta.
+    Route::match(['get', 'post'], '/user/profile', function () {
+        ...
+    });
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
+// Definición de una ruta tipo gett: Url , función
+// Esta estructura se puede denominar "Closure" sinonimo de una función callback
 Route::get('/', function () {
+    // Al usar view Laravel automaticamente se dirige a la carpeta resources/views
     return view('welcome');
 });
+
+// En el caso especifico de no realizar ninguna operación adicional entre la petición y la respuesta podemos utilizar el metodo "view" que responder a las peticiones de tipo "get" y "head"
+// Utilizando el metodo "name" le podemos asignar un "nombre" para instanciarlo con route() llamando su nombre y no la url propia.
+Route::view('/inicio', 'session')->name('sesion');

74738de107dab306566186808e0c0eb70c826500
MatMT <oscarmateoelias@gmail.com>
2022-06-20 21:32:44 -0600

    Documentación

--- Apollos-code/database/migrations/2014_10_12_000000_create_users_table.php
+++ Apollos-code/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id(); // Integer(Entero) - Unsigned(Sin signo) - Increment (Autoincrementable) 
+            $table->string('name'); // Varchar(250 caracteres máx)
+            $table->string('email')->unique(); // Adicionalmente se le agrega la propiedad que protege este campo como datos unicos a nivel de base de datos
+            $table->timestamp('email_verified_at')->nullable(); // Para guardar fecha de verificación, puede quedar vacio
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken(); // Varchar de 100 digitos para un token de validación al momento de iniciar sesión y mantenerla guardada
+            $table->timestamps(); // Crea dos columnas para verificar la fecha y hora de creación y actualización
+            // $table->text('parrafo'); // Textos largos 
         });
     }
 

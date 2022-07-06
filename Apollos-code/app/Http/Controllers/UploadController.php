679f4d59f08563f0829fe250d543698f694d3d81
MatMT <oscarmateoelias@gmail.com>
2022-07-06 15:58:11 -0600

    Vistas - Subir un archivo

--- Apollos-code/app/Http/Controllers/UploadController.php
+++ Apollos-code/app/Http/Controllers/UploadController.php
@@ -2,21 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 
-class ProfileController extends Controller
+class UploadController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
-    }
-
-    public function index(User $user)
-    {
-        return view('profile', [
-            'user' => $user
-        ]);
     }
 
     public function create()
---
+++ Apollos-code/resources/views/uploads/create.blade.php
@@ -0,0 +1,47 @@
+@extends('partials.nav_bar')
+
+@section('titulo')
+    Creando una nueva Publicación
+@endsection
+
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
+@section('contenido')
+    <div class="md:flex md:items-center">
+        <div class="md:w-1/2 px-10">
+            <form action="" method="POST" enctype="multipart/form-data" id="dropzone"
+                class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+                @csrf
+            </form>
+        </div>
+
+        <div class="md:w-1/2 p-10 bg-white rounded-lg shadow mt-10 md:mt-0">
+            <form action="" method="POST" novalidate>
+                @csrf
+                <div class="mb-5">
+                    <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">Titulo</label>
+                    <input type="text" id="titulo" name="titulo" placeholder="Tu titulo"
+                        class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror"
+                        value="{{ old('titulo') }}">
+                    @error('titulo')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">Descripción</label>
+                    <textarea id="descripcion" name="descripcion" placeholder="Descripción de la Publicación"
+                        class="border p-3 w-full rounded-lg @error('descripcion') border-red-500 @enderror"> {{ old('descripcion') }} </textarea>
+                    @error('descripcion')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <input type="submit" value="Publicar"
+                    class="bg-sky-600 transition-colors cursor-pointer uppercase font-bold w-full p-3  text-white rounded-lg">
+
+            </form>
+        </div>
+    </div>
+@endsection

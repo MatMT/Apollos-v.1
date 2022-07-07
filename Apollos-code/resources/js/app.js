27dc55dc714f9a3db827bc781b583c86813b7696
MatMT <oscarmateoelias@gmail.com>
2022-07-06 22:41:41 -0600

    Npm - Dropzone
    Agregando Dropzone al proyecto para la subida de archivos

--- Apollos-code/resources/js/app.js
+++ Apollos-code/resources/js/app.js
@@ -1,2 +1,14 @@
-import './bootstrap';
 import '../css/app.css';
+
+import Dropzone from 'dropzone';
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#dropzone', {
+    dictDefaultMessage: 'Sube aquí tu imagen',
+    acceptedFiles: '.png, .jpg, .jpeg',
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo',
+    maxFiles: 1,
+    uploadMultiple: false,
+});
--- Apollos-code/resources/views/partials/nav_bar.blade.php
+++ Apollos-code/resources/views/partials/nav_bar.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @stack('styles')
     <title>Apollo's - @yield('titulo')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>

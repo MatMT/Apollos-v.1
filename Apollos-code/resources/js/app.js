02dc28ccb5bfbe840ca78b0867c518f2f3072036
MatMT <oscarmateoelias@gmail.com>
2022-07-08 21:14:50 -0600

    Update app.js

--- Apollos-code/resources/js/app.js
+++ Apollos-code/resources/js/app.js
@@ -1,5 +1,13 @@
+// Archivos a compilar
 import '../css/app.css';
+import '../css/home.css';
+import '../css/session.css';
 
+//  !!! SOLUCIONAR !!!
+// import '../css/styles.css';
+// import '../css/tailwind.css';
+
+// Dropzone
 import Dropzone from 'dropzone';
 
 Dropzone.autoDiscover = false;
@@ -12,3 +20,16 @@ const dropzone = new Dropzone('#dropzone', {
     maxFiles: 1,
     uploadMultiple: false,
 });
+
+dropzone.on('sending', function (file, xhr, formData) {
+    console.log(file);
+})
+dropzone.on('success', function (file, response) {
+    console.log(response);
+})
+dropzone.on('error', function (file, message) {
+    console.log(message);
+})
+dropzone.on('removedfile', function () {
+
+})

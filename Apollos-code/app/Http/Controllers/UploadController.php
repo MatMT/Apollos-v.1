aab1f5d96a10dfaca210eb34f4858d1aa202c062
MatMT <oscarmateoelias@gmail.com>
2022-07-08 21:12:09 -0600

    Upload controller

--- Apollos-code/app/Http/Controllers/UploadController.php
+++ Apollos-code/app/Http/Controllers/UploadController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 use Illuminate\Http\Request;
 
 class UploadController extends Controller
@@ -14,5 +16,25 @@ class UploadController extends Controller
     public function create()
     {
         return view('uploads.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Imagen en memoria
+        $imagen = $request->file('file');
+
+        // Str::uuid() - Genera un id único para cada archivo
+        $nombreImagen = Str::uuid() . '.' . $imagen->extension();
+
+        // Imagen de Intervetion/Image
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000);
+
+        // Ruta de la imagen - public_path() - apunta hacia la carpeta public
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        // Respuesta obtenida del app.js (dropzone.on)
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }

1273002a30f5e0c16d697a5e65fded8cdd45832f
idfAero <blomiau48@gmail.com>
2022-08-01 13:55:27 -0600

    Cambio menor: Añadido favicon

--- Apollos-code/resources/views/auth/signup.blade.php
+++ Apollos-code/resources/views/auth/signup.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./assets/favicon.png" type="image/x-icon">
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{ asset('css/session.css') }}">
     <title>Regístrate | Apollo's</title>

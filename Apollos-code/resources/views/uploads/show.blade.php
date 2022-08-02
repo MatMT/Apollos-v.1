94b2d37de7715539ed6651b9c6457d4d769d808e
MatMT <oscarmateoelias@gmail.com>
2022-08-02 17:50:39 -0600

    Impresión de canción

--- Apollos-code/resources/views/uploads/show.blade.php
+++ Apollos-code/resources/views/uploads/show.blade.php
@@ -5,4 +5,24 @@
 @endsection
 
 @section('contenido')
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2 bg-black">
+
+        </div>
+        <div class="md:w-1/2">
+            <img class="w-full" src="{{ asset('storage') . '/uploads/imagenes/' . $song->image }}"
+                alt="Imagen de la canción {{ $song->name_song }}">
+        </div>
+    </div>
+    <div class="bg-slate-400 flex justify-center items-center">
+        <div class="w-1/4">
+            <span class=" block text-center">{{ $song->user->name }}</span>
+        </div>
+        <div class="w-3/4">
+            <audio controls class="w-full">
+                <source src="{{ asset('storage') . '/uploads/canciones/' . $song->url }}">
+            </audio>
+        </div>
+
+    </div>
 @endsection

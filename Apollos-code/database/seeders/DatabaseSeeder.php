d69f69aa0ef6b3d24e5725e97c5e5e6d11ba73ae
MatMT <oscarmateoelias@gmail.com>
2022-08-02 15:39:33 -0600

    Seeders

--- Apollos-code/database/seeders/DatabaseSeeder.php
+++ Apollos-code/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Song;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@correo.com',
-        // ]);
+        User::factory(10)->create();
+        Song::factory(50)->create();
     }
 }

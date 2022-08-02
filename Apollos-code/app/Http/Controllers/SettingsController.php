b5c18a02f9ffd988899b38e17eee2f150ca65019
MatMT <oscarmateoelias@gmail.com>
2022-08-01 18:49:13 -0600

    Error Ruta

--- Apollos-code/app/Http/Controllers/SettingsController.php
+++ Apollos-code/app/Http/Controllers/SettingsController.php
@@ -2,69 +2,61 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Auth; 
-use Illuminate\Support\Facades\Hash; 
-use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
-class UserSettings extends Controller
+class SettingsController extends Controller
 {
-    public function NewPassword(){
+    public function NewPassword()
+    {
         return view('configure_u ser_profile');
     }
 
-    
-    public function changePassword(Request $request){    
-        
+
+    public function changePassword(Request $request)
+    {
+
         $user           = Auth::user();
         $userId         = $user->id;
         $userEmail      = $user->email;
         $userPassword   = $user->password;
 
-        if($request->password_actual !=""){
+        if ($request->password_actual != "") {
             $NuewPass   = $request->password;
             $confirPass = $request->confirm_password;
             $name       = $request->name;
 
-                //Verifico si la clave actual es igual a la clave del usuario en session
-                if (Hash::check($request->password_actual, $userPassword)) {
+            //Verifico si la clave actual es igual a la clave del usuario en session
+            if (Hash::check($request->password_actual, $userPassword)) {
 
-                    //Valido que tanto la 1 como 2 clave sean iguales
-                    if($NuewPass == $confirPass){
-                        //Valido que la clave no sea Menor a 6 digitos
-                        if(strlen($NuewPass) >= 6){
-                            $user->password = Hash::make($request->password);
-                            $sqlBD = DB::table('users')
-                                  ->where('id', $user->id)
-                                  ->update(['password' => $user->password], ['name' => $user->name]);
-                    
-                            return redirect()->back()->with('updateClave','La clave fue cambiada correctamente.');
-                        }else{
-                            return redirect()->back()->with('clavemenor','Recuerde la clave debe ser mayor a 6 digitos.');
-                        }
-        
-                }else{
-                    return redirect()->back()->with('claveIncorrecta','Por favor verifique las claves no coinciden.');
+                //Valido que tanto la 1 como 2 clave sean iguales
+                if ($NuewPass == $confirPass) {
+                    //Valido que la clave no sea Menor a 6 digitos
+                    if (strlen($NuewPass) >= 6) {
+                        $user->password = Hash::make($request->password);
+                        $sqlBD = DB::table('users')
+                            ->where('id', $user->id)
+                            ->update(['password' => $user->password], ['name' => $user->name]);
+
+                        return redirect()->back()->with('updateClave', 'La clave fue cambiada correctamente.');
+                    } else {
+                        return redirect()->back()->with('clavemenor', 'Recuerde la clave debe ser mayor a 6 digitos.');
+                    }
+                } else {
+                    return redirect()->back()->with('claveIncorrecta', 'Por favor verifique las claves no coinciden.');
                 }
-           
-            }else{
-                return back()->withErrors(['password_actual'=>'La Clave no Coinciden']);
+            } else {
+                return back()->withErrors(['password_actual' => 'La Clave no Coinciden']);
             }
-
-
-        }else{
+        } else {
             $name       = $request->name;
             $sqlBDUpdateName = DB::table('users')
-                            ->where('id', $user->id)
-                            ->update(['name' => $name]);
-            return redirect()->back()->with('name','El nombre fue cambiado correctamente.');;
-
+                ->where('id', $user->id)
+                ->update(['name' => $name]);
+            return redirect()->back()->with('name', 'El nombre fue cambiado correctamente.');;
         }
-
-        
-
-}
-
+    }
 }

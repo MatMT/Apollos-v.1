8c8667525f4278bb47b382bf02e66e05c66f9937
MatMT <oscarmateoelias@gmail.com>
2022-08-02 12:01:01 -0600

    Update Song.php

--- Apollos-code/app/Models/Song.php
+++ Apollos-code/app/Models/Song.php
@@ -9,9 +9,6 @@ class Song extends Model
 {
     use HasFactory;
 
-    // Deshabilitado para las datos de prueba
-    public $timestamps = false;
-
     // Campos a recibir
     protected $fillable = [
         'name_song',

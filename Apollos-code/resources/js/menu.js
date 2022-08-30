b2c0963152771d0ba1a5d42c47532eafca2c9a24
MartinTorres987 <eduar10.mart@gmail.com>
2022-08-29 23:59:49 -0600

    Cambios Lista de reproducción

--- Apollos-code/resources/js/menu.js
+++ Apollos-code/resources/js/menu.js
@@ -31,12 +31,32 @@ pantalla.addEventListener('dblclick', ()=>{
 const div_player = document.getElementById("player");
 const btn_show = document.getElementById("show");
 const btn_hidde = document.getElementById("hidde");
+const box_con_list = document.getElementById("list");
+const lista_can = document.getElementById("canciones-lista");
 
 btn_show.addEventListener('click', function(){
     div_player.classList.add("activer");
+
+    setTimeout(() => {
+        box_con_list.classList.add("active");
+        lista_can.classList.add("active");
+    }, 500);
+
+    box_con_list.classList.add("largo");
+    
 })
 
 btn_hidde.addEventListener('click', function(){
-    div_player.classList.remove("activer"); 
+    
+    
+    box_con_list.classList.remove("active");
+    lista_can.classList.remove("active");
+
+    setTimeout(() => {
+        div_player.classList.remove("activer");
+        box_con_list.classList.remove("largo");
+        
+    }, 500);
+    
        
 })
